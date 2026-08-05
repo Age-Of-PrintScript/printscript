254114e3fc20cbc7a2cc362a6e919b0628924c05
Facugarciar0805 <facugarciar@gmail.com>
2026-08-05 20:39:24 -0300

    fix boludes
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- parser/src/main/kotlin/ParserImpl.kt
+++ parser/src/main/kotlin/ParserImpl.kt
@@ -1,5 +1,5 @@
 class ParserImpl : Parser {
-    override fun parse(tokens: List<Token>): AST {
+    override fun parse(tokens: List<Token>): Program {
         TODO("Not yet implemented")
     }
 }
