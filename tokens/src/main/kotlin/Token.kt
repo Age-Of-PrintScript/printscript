c904e6d7636deaa09c906dcab3becaf5b2b6960d
Facugarciar0805 <facugarciar@gmail.com>
2026-08-05 19:01:07 -0300

    hola

---
+++ tokens/src/main/kotlin/Token.kt
@@ -0,0 +1,1 @@
+data class Token(val type: TokenType, val start: Position, val end: Position)
---
+++ tokens/src/main/kotlin/TokenType.kt
@@ -0,0 +1,19 @@
+sealed interface TokenType {
+    object LET: TokenType
+    object COLON: TokenType
+    object SEMICOLON: TokenType
+    object ASSIGN: TokenType
+    object NUMBER_TYPE: TokenType
+    object STRING_TYPE: TokenType
+}
+
+enum class Operator: TokenType {
+    SUM, SUBSTRACT, MULTIPLY, DIVIDE
+}
+
+data class Identifier(val name: String): TokenType
+
+data class NumberLiteral(val number: Number): TokenType
+
+data class StringLiteral(val string: String): TokenType
+
