d977b03ed7abf3560bc0d2abe2f1a5317506cc15
nachogarcia1 <igarcia1@mail.austral.edu.ar>
2026-08-10 22:41:02 -0300

    Creacion del expression parser (#13)
    * avances del RHS del parser. Todavía no compila y no fue testeado.
    
    * ahora compila pero falta enchufar la expression al AST.
    
    Tambien falta testear
    
    * hice una especie de tests de las operaciones que escupe el expression parser para ver si se respeta la precedencia en las operaciones.
    
    Falta definir program
    
    * Remove out modifier from ParseResult type parameter
    
    * usando expression correctamente desde /ast y arreglado tipado en tests
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * arrego cositas de copilot
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    ---------
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    Co-authored-by: Facugarciar0805 <facugarciar@gmail.com>
    Co-authored-by: Facundo García Rosselli <162722335+Facugarciar0805@users.noreply.github.com>

---
+++ parser/src/test/kotlin/ExpressionSolver.kt
@@ -0,0 +1,29 @@
+
+internal class ExpressionSolver { //clase auxiliar para hacer tests mas faciles. Solo sirve para expresiones sin variables
+
+    internal fun solve(expression: Expression): Number = when (expression) {
+            is Expression.Literal ->  {
+                when (val scriptValue = expression.value){
+                    is PrintScriptValue.NumberLiteral -> scriptValue.value
+                    is PrintScriptValue.StringLiteral -> TODO()
+                }
+            }
+            is Expression.Variable -> throw IllegalArgumentException("Unexpected expression")
+            is Expression.Operation -> solveOperation(expression)
+            }
+
+
+    private fun solveOperation(operation: Expression.Operation): Number {
+        val left = solve(operation.left).toDouble()
+        val right = solve(operation.right).toDouble()
+
+        return when (operation.operator) {
+            PrintScriptOperator.SUM -> left + right
+            PrintScriptOperator.SUBTRACT -> left - right
+            PrintScriptOperator.MULTIPLY -> left * right
+            PrintScriptOperator.DIVIDE -> left / right
+        }
+
+    }
+
+}
---
+++ parser/src/test/kotlin/ExpressionTest.kt
@@ -0,0 +1,65 @@
+import kotlin.test.assertEquals
+import kotlin.test.Test
+
+typealias Num = PrintScriptValue.NumberLiteral
+
+class ExpressionTest {
+
+    private val solver = ExpressionSolver()
+
+
+    private fun assertExpressionEquals(expression: Expression, expectedValue: Number) {
+        assertEquals(expectedValue.toDouble(), solver.solve(expression))
+    }
+
+    @Test
+    fun `la multiplicacion tiene precedencia sobre la suma`() {
+        // 2 + (3 * 4) = 14, no (2 + 3) * 4 = 20
+        val expression = Expression.Operation(
+            left = Expression.Literal(Num(2)),
+            operator = PrintScriptOperator.SUM,
+            right = Expression.Operation(
+                Expression.Literal(Num(3)),
+                    Expression.Literal(Num(4)),
+                    PrintScriptOperator.MULTIPLY)
+        )
+        assertExpressionEquals(expression, 14)
+    }
+
+    @Test
+    fun `suma simple`() {
+        val expression = Expression.Operation(
+            Expression.Literal(Num(2)),
+        Expression.Literal(Num(3)),
+            PrintScriptOperator.SUM
+        )
+        assertExpressionEquals(expression, 5)
+    }
+
+    @Test
+    fun `resta simple`() {
+        val expression = Expression.Operation(
+            Expression.Literal(Num(10)),
+            Expression.Literal(Num(4)),
+            PrintScriptOperator.SUBTRACT
+        )
+        assertExpressionEquals(expression, 6)
+    }
+
+    @Test
+    fun `division simple`() {
+        val expression = Expression.Operation(
+            Expression.Literal(Num(20)),
+            Expression.Literal(Num(5)),
+            PrintScriptOperator.DIVIDE
+        )
+        assertExpressionEquals(expression, 4)
+
+    }
+
+
+}
+
+
+
+
