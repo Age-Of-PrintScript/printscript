492fe90122e5fd26eee3a07270ea15b3a3ef4bc4
luciabetelu <lubetelu@gmail.com>
2026-08-12 18:21:27 -0300

    arreglo errores

--- parser/src/main/kotlin/parser/Error.kt
+++ parser/src/main/kotlin/parser/Error.kt
@@ -2,7 +2,7 @@ package parser
 
 import domain.Error
 
-enum class SyntaxError(message: String): Error {
+enum class SyntaxError(val message: String): Error {
     MISSING_COLON_IN_DECLARATION("Missing colon in declaration"),
     MISSING_ASSIGNMENT_OPERATOR("Expected assignment"),
     MISSING_SEMICOLON("Expected semicolon"),
@@ -10,7 +10,7 @@ enum class SyntaxError(message: String): Error {
     MISSING_TYPE_IN_DECLARATION("Missing type in declaration"),
     MISSING_FUNCTION_NAME("Missing function name in call"),
     INVALID_TOKEN_AFTER_TYPE("Expected '=' or ';' after type declaration"),
-    INVALID_TOKEN("Unexpected token in expression"),
+    INVALID_TOKEN("Unexpected token"),
     INCOMPLETE_STATEMENT("Unexpected end of sentence, incomplete statement"),
     UNEXPECTED_TOKEN_AFTER_STATEMENT("Unexpected token after end of statement, expected semicolon"),
     WRONG_TOKEN_TYPE("Number or variable was expected")
