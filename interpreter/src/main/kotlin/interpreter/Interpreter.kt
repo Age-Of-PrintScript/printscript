93859ad4083a7bb7f000162d5f5f53812ab0dca8
Facugarciar0805 <facugarciar@gmail.com>
2026-08-12 16:28:07 -0300

    interpreter interface defined
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- interpreter/src/main/kotlin/interpreter/Interpreter.kt
+++ interpreter/src/main/kotlin/interpreter/Interpreter.kt
@@ -5,11 +5,11 @@ import ast.Program
 
 //TODO -> Cambiar return horrible que pusimos
 interface Interpreter {
-    fun execute(program: Program): Either<Unit, Unit>
+    fun execute(program: Program): Either<RuntimeError, ExecutionResult>
 }
 
 class InterpreterImpl: Interpreter {
-    override fun execute(program: Program): Either<Unit, Unit> {
+    override fun execute(program: Program): Either<RuntimeError, ExecutionResult> {
         TODO("Not yet implemented")
     }
 }
