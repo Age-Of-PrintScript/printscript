c90842f944142dbb5e7997ba0c2f170616cef29a
luciabetelu <lubetelu@gmail.com>
2026-08-18 19:35:01 -0300

    agrego funcion de extension para el Either 'getOrReturn'

--- domain/src/main/kotlin/domain/Result.kt
+++ domain/src/main/kotlin/domain/Result.kt
@@ -4,3 +4,11 @@ sealed interface Either<L, R>
 
 data class Failure<L, R>(val value: L) : Either<L, R>
 data class Success<L, R>(val value: R) : Either<L, R>
+
+// Esta funcion esta explicada en los logs
+inline fun <L, R> Either<L, R>.getOrReturn(onFailure: (L) -> Nothing): R {
+    return when (this) {
+        is Failure -> onFailure(this.value)
+        is Success -> this.value
+    }
+}
