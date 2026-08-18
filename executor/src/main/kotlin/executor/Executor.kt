a60e7ea300341447d3e977944879136e6b5a7f4d
Facugarciar0805 <facugarciar@gmail.com>
2026-08-18 14:38:12 -0300

    HOTFIX: lexer shouldnt be internal
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- executor/src/main/kotlin/executor/Executor.kt
+++ executor/src/main/kotlin/executor/Executor.kt
@@ -1,4 +1,14 @@
 package executor
 
+import interpreter.ExecutionResult
+import interpreter.InterpreterImpl
+import lexer.Lexer
+
+
 class Executor {
+    private val lexer: Lexer = Lex()
+    fun execute(source: String): ExecutionResult{
+
+
+    }
 }
