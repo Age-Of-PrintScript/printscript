8deb5d6a0c1caa93fe6890edd99401f82f7cd0a8
Facugarciar0805 <facugarciar@gmail.com>
2026-08-18 14:47:08 -0300

    terminado el executor
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- executor/src/main/kotlin/executor/Executor.kt
+++ executor/src/main/kotlin/executor/Executor.kt
@@ -1,14 +1,31 @@
 package executor
 
+import domain.Either
+import domain.Failure
+import domain.Error
+import domain.Success
 import interpreter.ExecutionResult
+import interpreter.Interpreter
 import interpreter.InterpreterImpl
 import lexer.Lexer
+import lexer.LexerImpl
+import parser.Parser
+import parser.ParserImpl
 
 
 class Executor {
-    private val lexer: Lexer = Lex()
-    fun execute(source: String): ExecutionResult{
-
-
+    private val lexer: Lexer = LexerImpl()
+    private val parser: Parser = ParserImpl()
+    private val interpreter: Interpreter = InterpreterImpl()
+    fun execute(source: String): Either<Error, ExecutionResult> {
+        val tokens = lexer.tokenize(source)
+        if(tokens is Failure) return Failure(tokens.value)
+        val program = parser.parse((tokens as Success).value)
+        if(program is Failure) return Failure(program.value)
+        val executionResult = interpreter.execute((program as Success).value)
+        return when(executionResult) {
+            is Failure -> Failure(executionResult.value)
+            is Success -> Success(executionResult.value)
+        }
     }
 }
