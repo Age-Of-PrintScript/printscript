4d2d014ab955a2711711e56219d062b822f99bae
Facugarciar0805 <facugarciar@gmail.com>
2026-08-26 15:45:51 -0300

    mejoro engine y creo validate function
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- engine/src/main/kotlin/executor/Engine.kt
+++ engine/src/main/kotlin/executor/Engine.kt
@@ -1,9 +1,11 @@
 package executor
 
+import ast.Program
 import domain.Either
 import domain.Error
 import domain.Failure
 import domain.Success
+import domain.getOrReturn
 import interpreter.ExecutionResult
 import interpreter.Interpreter
 import interpreter.InterpreterImpl
@@ -18,14 +20,18 @@ class Engine {
     private val interpreter: Interpreter = InterpreterImpl()
 
     fun execute(source: String): Either<Error, ExecutionResult> {
-        val tokens = lexer.tokenize(source)
-        if (tokens is Failure) return Failure(tokens.value)
-        val program = parser.parse((tokens as Success).value)
-        if (program is Failure) return Failure(program.value)
-        val executionResult = interpreter.execute((program as Success).value)
+        val tokens = lexer.tokenize(source).getOrReturn { return Failure(it) }
+        val program = parser.parse(tokens).getOrReturn { return Failure(it) }
+        val executionResult = interpreter.execute(program)
         return when (executionResult) {
             is Failure -> Failure(executionResult.value)
             is Success -> Success(executionResult.value)
         }
+    }
+
+    fun validate(source: String): Either<Error, Program> {
+        val tokens = lexer.tokenize(source).getOrReturn { return Failure(it) }
+        val program = parser.parse(tokens).getOrReturn { return Failure(it) }
+        return Success(program)
     }
 }
