629b0a86cd615337b7a33dd300561d19e2bad704
Facugarciar0805 <facugarciar@gmail.com>
2026-09-02 16:24:53 -0300

    optimización de convention rules (en ves de instanciar el regex cada vez, lo dejo instanciado y simplemente lo paso (no importa)
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- lexer/src/main/kotlin/lexer/Error.kt
+++ lexer/src/main/kotlin/lexer/Error.kt
@@ -1,19 +1,15 @@
 package lexer
+
 import domain.Error
 
 enum class LexerError(
-    val mensaje: String,
+    private val message: String,
 ) : Error {
-    INVALID_CHARACTER("Invalid character") {
-        override fun getMessage() = mensaje
-    },
-    INVALID_CHARACTER_FOR_TOKEN_TYPE("Invalid character for current token type") {
-        override fun getMessage() = mensaje
-    },
-    UNTERMINATED_STRING("Unterminated string literal") {
-        override fun getMessage() = mensaje
-    },
-    UNDETERMINED_TOKEN_TYPE("Could not determine token type") {
-        override fun getMessage() = mensaje
-    },
+    INVALID_CHARACTER("Invalid character"),
+    INVALID_CHARACTER_FOR_TOKEN_TYPE("Invalid character for current token type"),
+    UNTERMINATED_STRING("Unterminated string literal"),
+    UNDETERMINED_TOKEN_TYPE("Could not determine token type"),
+    ;
+
+    override fun getMessage(): String = message
 }
