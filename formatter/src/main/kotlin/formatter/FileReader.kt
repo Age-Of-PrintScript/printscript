8df5cc48b4a52541e3539c4542c9c2722f8e5570
Facugarciar0805 <facugarciar@gmail.com>
2026-09-03 10:29:52 -0300

    saco unused fileReaderFake
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- formatter/src/main/kotlin/formatter/FileReader.kt
+++ formatter/src/main/kotlin/formatter/FileReader.kt
@@ -1,13 +1,5 @@
 package formatter
 
-import kotlinx.serialization.json.JsonNull.content
-
 interface FileReader {
     fun readText(path: String): String
-}
-
-class FileReaderFake(
-    content: String,
-) : FileReader {
-    override fun readText(path: String): String = content
 }
