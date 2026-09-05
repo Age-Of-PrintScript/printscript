c5061cfc7f8c33fb5676d80cf97920007749c62b
Facugarciar0805 <facugarciar@gmail.com>
2026-09-05 13:35:35 -0300

    creo comando validate
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- cli/src/main/kotlin/cli/Main.kt
+++ cli/src/main/kotlin/cli/Main.kt
@@ -1,7 +1,20 @@
 package cli
 
 import cli.commands.RunCommand
+import cli.commands.ValidateCommand
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
+
+class PrintScriptCli :
+    CliktCommand(
+        name = "printscript",
+        help = "CLI para ejecutar y validar código PrintScript",
+    ) {
+    override fun run() = Unit
+}
 
 fun main(args: Array<String>) {
-    RunCommand().main(args)
+    PrintScriptCli()
+        .subcommands(RunCommand(), ValidateCommand())
+        .main(args)
 }
---
+++ cli/src/main/kotlin/cli/commands/ValidateCommand.kt
@@ -0,0 +1,23 @@
+package cli.commands
+
+import cli.adapters.ConsoleLogger
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.help
+import com.github.ajalt.clikt.parameters.types.file
+import executor.Engine
+import executor.Logger
+
+class ValidateCommand(
+    private val engine: Engine = Engine(),
+    private val logger: Logger = ConsoleLogger(),
+) : CliktCommand(name = "validate", help = "Valida que un script PrintScript este bien escrito") {
+    private val file by argument()
+        .file(mustExist = true, canBeFile = true, canBeDir = false, mustBeWritable = false, mustBeReadable = true)
+        .help("Ruta al archivo .ps a validar")
+
+    override fun run() {
+        val source = file.readText()
+        engine.validate(source, logger)
+    }
+}
