fd70e1f2ae4b76463a154baac0e8713b133d81a4
Facugarciar0805 <facugarciar@gmail.com>
2026-09-13 16:42:48 -0300

    corrijo error de compilacion
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- cli/src/main/kotlin/cli/commands/Lint.kt
+++ cli/src/main/kotlin/cli/commands/Lint.kt
@@ -3,22 +3,18 @@ package cli.commands
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.help
-import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
 import linter.Linter
 
-class Lint(
-    private val linter: Linter = Linter.createDefault(),
-) : CliktCommand(name = "lint", help = "Analiza un script PrintScript siguiendo las convenciones de codigo") {
+class Lint : CliktCommand(name = "lint", help = "Analiza un script PrintScript siguiendo las convenciones de codigo") {
     private val file by argument()
         .file(mustExist = true, canBeFile = true, canBeDir = false, mustBeWritable = false, mustBeReadable = true)
         .help("Ruta al archivo .ps a analizar")
 
-    private val version by option("-v", "--version", help = "Versión de PrintScript")
-
     override fun run() {
+        val linter: Linter = Linter.createDefault()
         val source = file.readText()
-        val warnings = linter.analyse(source, version = version)
+        val warnings = linter.analyse(source)
         if (warnings.isNotEmpty()) {
             println(warnings.joinToString("\n"))
         } else {
