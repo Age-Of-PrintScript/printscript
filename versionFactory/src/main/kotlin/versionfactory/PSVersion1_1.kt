4568ba6c2bb41c47a83a989c9a3594477bb78052
Facundo García Rosselli <162722335+Facugarciar0805@users.noreply.github.com>
2026-09-13 13:02:18 -0300

    Feat: 1.1 al parser (#99)
    * agregado manejo de versiones de parser test framework
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * agregado soporte para tickets
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * terminado if builder con mini refactor
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * devuelvo el contrato del .new() asi no se rompe todo
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * terminados los ultimos casos unitarios de test
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * agregado soporte para nested ifs
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    ---------
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- versionFactory/src/main/kotlin/versionfactory/PSVersion1_1.kt
+++ versionFactory/src/main/kotlin/versionfactory/PSVersion1_1.kt
@@ -7,20 +7,24 @@ import interpreter.OperationKey
 import interpreter.statement.StatementEvaluator
 import parser.builders.StatementParser
 import tokens.TokenType
+import versionfactory.v1_0.v1_0BinaryOperations
+import versionfactory.v1_0.v1_0builtInFunctions
 import versionfactory.v1_1.v1_1Keywords
+import versionfactory.v1_1.v1_1StatementEvaluators
+import versionfactory.v1_1.v1_1StatementParsers
 import versionfactory.v1_1.v1_1Symbols
 
 internal class PSVersion1_1 : PSVersion {
     override val binaryOperations: Map<OperationKey, BinaryOperation>
-        get() = TODO("Not yet implemented")
+        get() = v1_0BinaryOperations
     override val builtInFunctions: Map<String, BuiltInFunction>
-        get() = TODO("Not yet implemented")
+        get() = v1_0builtInFunctions
     override val keywords: Map<String, TokenType>
         get() = v1_1Keywords
     override val symbols: Map<Char, TokenType>
         get() = v1_1Symbols
     override val statementParsers: List<StatementParser>
-        get() = TODO("Not yet implemented")
+        get() = v1_1StatementParsers
     override val statementEvaluators: Map<ASTType, StatementEvaluator>
-        get() = TODO("Not yet implemented")
+        get() = v1_1StatementEvaluators
 }
--- versionFactory/src/main/kotlin/versionfactory/v1_1/ValidAst.kt
+++ versionFactory/src/main/kotlin/versionfactory/v1_1/ValidAst.kt
@@ -2,7 +2,9 @@ package versionfactory.v1_1
 
 import ast.ASTType
 import interpreter.statement.StatementEvaluator
+import parser.BlockParser
 import parser.ExpressionParser
+import parser.builders.ConditionalParser
 import parser.builders.StatementParser
 import versionfactory.v1_0.v1_0StatementEvaluators
 import versionfactory.v1_0.v1_0StatementParsers
@@ -10,7 +12,14 @@ import versionfactory.v1_0.v1_0StatementParsers
 internal val expressionParser = ExpressionParser()
 
 internal val v1_1StatementParsers: List<StatementParser> =
-    v1_0StatementParsers
+    run {
+        val parsers = mutableListOf<StatementParser>()
+        val blockParser = BlockParser(parsers)
+        val conditionalParser = ConditionalParser(blockParser, expressionParser)
+        parsers.addAll(v1_0StatementParsers)
+        parsers.add(conditionalParser)
+        parsers
+    }
 
 internal val v1_1StatementEvaluators: Map<ASTType, StatementEvaluator> =
     v1_0StatementEvaluators
