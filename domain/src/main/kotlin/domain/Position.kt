a7c4ac85eab353c34529ff9209eaf6e09cd13a92
Facugarciar0805 <facugarciar@gmail.com>
2026-09-15 10:23:48 -0300

    feat(domain): add position metadata contract to Error and Position helpers
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- domain/src/main/kotlin/domain/Position.kt
+++ domain/src/main/kotlin/domain/Position.kt
@@ -3,4 +3,9 @@ package domain
 data class Position(
     val line: Int,
     val charPosition: Int,
-)
+) {
+    companion object {
+        val START = Position(1, 1)
+        val UNKNOWN = Position(0, 0)
+    }
+}
