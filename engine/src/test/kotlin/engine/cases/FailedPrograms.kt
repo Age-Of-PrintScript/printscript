bd39cbd468131de5bce4f4be865db36d47e2536d
Facundo García Rosselli <162722335+Facugarciar0805@users.noreply.github.com>
2026-09-15 10:15:15 -0300

    Feat: e2e testing for 1.1 (#122)
    * agregados multiples casos de test end to end usando printscipt 1.1
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * arreglo otro bug del formatter
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * set default to version 1.1
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * arreglos varios
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    * ponele
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>
    
    ---------
    
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- engine/src/test/kotlin/engine/cases/FailedPrograms.kt
+++ engine/src/test/kotlin/engine/cases/FailedPrograms.kt
@@ -21,5 +21,58 @@ object FailedPrograms {
                 "runtime error cuts off pipeline on type mismatch",
                 "let x: number = \"text\";",
             ),
+            // Version 1.1 Cases
+            FailureCase(
+                "v1.1 runtime error on reassignment to const variable",
+                "const x: number = 10;\nx = 20;",
+                version = "1.1",
+            ),
+            FailureCase(
+                "v1.1 runtime error on boolean type mismatch in declaration",
+                "let flag: boolean = 123;",
+                version = "1.1",
+            ),
+            FailureCase(
+                "v1.1 runtime error on boolean type mismatch in reassignment",
+                "let flag: boolean = true;\nflag = \"false\";",
+                version = "1.1",
+            ),
+            FailureCase(
+                "v1.1 runtime error when if condition is not boolean",
+                "if (42) {\nprintln(\"Invalid\");\n}",
+                version = "1.1",
+            ),
+            FailureCase(
+                "v1.1 runtime error on readInput invalid cast to number",
+                "let age: number = readInput(\"Age: \");",
+                version = "1.1",
+                inputs = listOf("not-a-number"),
+            ),
+            FailureCase(
+                "v1.1 runtime error on readInput invalid cast to boolean",
+                "let flag: boolean = readInput(\"Flag: \");",
+                version = "1.1",
+                inputs = listOf("not-a-boolean"),
+            ),
+            FailureCase(
+                "v1.1 runtime error on missing environment variable",
+                "let secret: string = readEnv(\"NON_EXISTENT_VAR\");",
+                version = "1.1",
+            ),
+            FailureCase(
+                "v1.1 runtime error on readEnv without arguments",
+                "let secret: string = readEnv();",
+                version = "1.1",
+            ),
+            FailureCase(
+                "v1.0 fails on v1.1 const keyword",
+                "const x: number = 10;",
+                version = "1.0",
+            ),
+            FailureCase(
+                "v1.0 fails on v1.1 if keyword",
+                "if (true) {\nprintln(1);\n}",
+                version = "1.0",
+            ),
         )
 }
